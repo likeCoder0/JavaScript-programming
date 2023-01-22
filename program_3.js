cc1e7123e98432a12736a76db05a473b6e374de4
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-22 20:10:54 +0530

    let learn js

---
+++ program_3.js
@@ -0,0 +1,20 @@
+console.log('var, let and const')
+var a=65
+{
+    var a="how are you?"
+    console.log(a)
+}
+console.log(a)
+
+let b=20
+let c=null
+let d=undefined
+{
+    let b="I am fine"
+    console.log(b)
+}
+console.log(b)
+// const hello_error;//not initial error
+const author="anchal"
+// let author="find"//this will throw an error
+// author=5//this will throw an error
---
+++ program_4.js
@@ -0,0 +1,21 @@
+//nn ss bb u - primitive datatype in js
+let a=null;
+let b=20;
+let c=true;
+let d=BigInt("583")+BigInt("5");
+let e="Anchal"
+let f=Symbol("i am fine")
+let g
+console.log(a, b, c, d, e, f, g)
+console.log(typeof d)
+
+//object in js
+const item={
+    "Anchal":true,
+    "neha":false,
+    "Lovish":21,
+    "Rahul":undefined,
+}
+console.log(item["Anchal"])
+console.log(item["find"])
+console.log(item["Rahul"])
