5713e4abf29a8da8526fed508189a4027cf6c50c
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-22 23:24:22 +0530

    while loop and do while loop

---
+++ program_10.js
@@ -0,0 +1,12 @@
+let n=prompt("Enter the number you want print.");
+n=Number.parseInt(n);
+let i=0;
+//condition when true then print
+// while(i<n){
+//     console.log(i);
+//     i++;
+// }
+do{
+    console.log(i);
+    i++;
+}while(i<n);
