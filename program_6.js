874db9f50e530a663ebe69c517d6959d0c48b326
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-22 21:20:49 +0530

    operators

---
+++ program_6.js
@@ -0,0 +1,24 @@
+let a=99;
+let b=4;
+console.log("a+b="+(a+b));
+console.log("a-b="+(a-b));
+console.log("a*b="+(a*b));
+console.log("a/b="+(a/b));
+console.log("a%b="+(a%b));
+console.log("a**b="+(a**b));//a power b
+a++;
+console.log("a++="+(a));
+a--;
+console.log("a--="+(a));
+console.log("++a="+(++a));
+a+=2;
+console.log(a)
+let comp1=5;
+let comp2=4;
+console.log(comp1==comp2);
+console.log(comp1!=comp2);
+comp2="5";
+console.log('comp1===comp2:',comp1===comp2);
+console.log('comp1!==comp2:',comp1!==comp2);
+console.log('comp1===comp2:',comp1==comp2);
+console.log('comp1!==comp2:',comp1!=comp2);
