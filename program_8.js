114cee24fd37723e4597dc45e31c5592e7292476
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-22 22:34:39 +0530

    fine

---
+++ program_8.js
@@ -0,0 +1,9 @@
+let age=prompt("Enter your age?");
+switch(age){
+    case 16:
+        console.log("you age is ",age);
+        break;
+    default:
+        console.log("guess the age.");
+        break;
+}
