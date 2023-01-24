7e3a11f0453a81f9fc7e702c51e7e86526e7e0cc
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-24 16:49:09 +0530

    arrays

---
+++ program_16.js
@@ -0,0 +1,16 @@
+let marks_class_12=[83,95,74,85,false,"Not Present"];
+console.log(marks_class_12);
+console.log(marks_class_12[0]);
+console.log(marks_class_12[1]);
+console.log(marks_class_12[2]);
+console.log(marks_class_12[3]);
+console.log(marks_class_12[4]);
+console.log(marks_class_12[5]);
+console.log(marks_class_12[6]);
+console.log("The length of marks_class_12 is",marks_class_12.length);
+marks_class_12[6]=87;//Adding a new value in array
+console.log(marks_class_12[6]);
+marks_class_12[0]=97;//change the value in array
+console.log(marks_class_12)
+console.log(typeof marks_class_12)
+//Arrays is mutable it can change
