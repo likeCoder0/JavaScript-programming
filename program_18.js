e5ce8b8068653388c5f71b78f4cee5a57ab32d5c
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-24 17:30:55 +0530

    arrays method

---
+++ program_18.js
@@ -0,0 +1,21 @@
+// let num=[5,3,6,2,6,3];
+// let num_more=[15,13,16,12,16,31];
+// let num_even_more=[152,123,216,122,216,321];
+// console.log(num.length);
+// delete num[0];
+// console.log(num);
+// console.log(num.length);
+// let newArray=num.concat(num_more,num_even_more);
+// console.log(newArray);
+let compare=(a,b)=>{
+    return a-b;//you want in descending order then return b-a
+}
+//sort
+let a=[3,6,94,83,55,22,8,4,34,23,15];
+// a.sort(compare);
+// a.reverse();
+// let deleteValues=a.splice(2,3,1023,1053,1923,1853);
+// console.log(a);
+// console.log(deleteValues);
+let Newarr=a.slice(4,7);
+console.log(Newarr)
