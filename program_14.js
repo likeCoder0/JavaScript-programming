ab8c3446c714217dfe963b82883027251dd02efb
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-24 13:39:00 +0530

    string function

---
+++ program_14.js
@@ -0,0 +1,13 @@
+let name="Anchal";
+console.log(name.length);
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2,4));
+console.log(name.slice(2));
+let friend="Harsh"
+console.log(name.replace("An","Fi"));
+console.log(name.concat("is a friend of",friend,". Ok fine"));
+let friend2="     Jay       ";
+console.log(friend2);
+console.log(friend2.trim());
+//string is immutable cannot be change
