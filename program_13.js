4040d3ac512998b29cc6a85838fb2c3cc4e1fb55
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-24 13:03:41 +0530

    strings

---
+++ program_13.js
@@ -0,0 +1,13 @@
+const name="Anchal";
+console.log(name.length);
+console.log(name[0]);
+console.log(name[1]);
+console.log(name[2]);
+
+//template literals
+let name1="Jay";
+let sentence=`${name1} is friend of ${name}`;
+console.log(sentence);
+
+let word='don\'t';
+console.log(word);
