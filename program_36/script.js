f90fcb657138bc6249eee9844ccf4d9b2595e5d5
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-26 13:33:02 +0530

    dom search

---
+++ program_36/script.js
@@ -0,0 +1,16 @@
+//change the color of card titile
+let ctitle=document.getElementById("card_first_title");
+ctitle.style.color="red";
+
+let ctitles=document.querySelectorAll(".card-title");
+ctitles[0].style.color="blue";
+ctitles[1].style.color="red";
+ctitles[2].style.color="green";
+console.log(ctitles);
+
+document.querySelector(".this").style.color="cyan";
+document.querySelector(".this").style.background="red";
+
+console.log(document.getElementsByTagName("a"))
+console.log(document.querySelector(".card").getElementsByTagName("a"))
+console.log(document.getElementsByName("Search"))
