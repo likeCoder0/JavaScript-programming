ac44374e2f321b15002736ad03707844d62dfb49
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-26 15:52:51 +0530

    joke

--- random_joke/script.js
+++ random_joke/script.js
@@ -1,309 +1,313 @@
 let jokes=[
-`What’s the best thing about Switzerland?
+`What’s the best thing about Switzerland?<br>
 I don’t know, but the flag is a big plus.`,
 
-`I invented a new word!
+`I invented a new word!<br>
 Plagiarism!`,
 
-`Did you hear about the mathematician who’s afraid of negative numbers?
+`Did you hear about the mathematician who’s afraid of negative numbers?<br>
 He’ll stop at nothing to avoid them.`,
 
-`Why do we tell actors to “break a leg?”
+`Why do we tell actors to “break a leg?”<br>
 Because every play has a cast. Here are some dark jokes to check out if you have a morbid sense of humor.`,
 
-`Helvetica and Times New Roman walk into a bar.
+`Helvetica and Times New Roman walk into a bar.<br>
 “Get out of here!” shouts the bartender. “We don’t serve your type.”`,
 
-`Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?”
+`Yesterday I saw a guy spill all his Scrabble letters on the road. I asked him, “What’s the word on the street?”<br>
 Once my dog ate all the Scrabble tiles. For days he kept leaving little messages around the house. Don’t miss these hilarious egg puns that will absolutely crack you up.`,
 
-`Knock! Knock!
-Who’s there?
-Control Freak.
-Con…
+`Knock! Knock!<br>
+Who’s there?<br>
+Control Freak.<br>
+Con…<br>
 OK, now you say, “Control Freak who?” `,
 
-`Hear about the new restaurant called Karma?
+`Hear about the new restaurant called Karma?<br>
 There’s no menu: You get what you deserve.`,
 
-`A woman in labor suddenly shouted, “Shouldn’t! Wouldn’t! Couldn’t! Didn’t! Can’t!”
+`A woman in labor suddenly shouted, “Shouldn’t! Wouldn’t! Couldn’t! Didn’t! Can’t!”<br>
 “Don’t worry,” said the doc. “Those are just contractions.”`,
 
-`A bear walks into a bar and says, “Give me a whiskey and … cola.”
+`A bear walks into a bar and says, “Give me a whiskey and … cola.”<br>
 “Why the big pause?” asks the bartender. The bear shrugged. “I’m not sure; I was born with them.”`,
 
-`Did you hear about the actor who fell through the floorboards?
+`Did you hear about the actor who fell through the floorboards?<br>
 He was just going through a stage.`,
 
-`Did you hear about the claustrophobic astronaut?
+`Did you hear about the claustrophobic astronaut?<br>
 He just needed a little space.`,
 
-`Why don’t scientists trust atoms?
+`Why don’t scientists trust atoms?<br>
 Because they make up everything.`,
 
-`Why did the chicken go to the séance?
+`Why did the chicken go to the séance?<br>
 To get to the other side. Check out these other “why did the chicken cross the road?” jokes for more laughs.`,
 
-`Where are average things manufactured?
+`Where are average things manufactured?<br>
 The satisfactory.`,
 
-`How do you drown a hipster?
+`How do you drown a hipster?<br>
 Throw him in the mainstream.`,
 
-`What sits at the bottom of the sea and twitches?
+`What sits at the bottom of the sea and twitches?<br>
 A nervous wreck.`,
 
-`What does a nosy pepper do?
+`What does a nosy pepper do?<br>
 Gets jalapeño business!`,
 
-`How does Moses make tea?
+`How does Moses make tea?<br>
 He brews.`,
 
-`Why can’t you explain puns to kleptomaniacs?
+`Why can’t you explain puns to kleptomaniacs?<br>
 They always take things literally.`,
 
-`How do you keep a bagel from getting away?
+`How do you keep a bagel from getting away?<br>
 Put lox on it.`,
 
-`A man tells his doctor, “Doc, help me. I’m addicted to Twitter!”
+`A man tells his doctor, “Doc, help me. I’m addicted to Twitter!”<br>
 The doctor replies, “Sorry, I don’t follow you …”`,
 
-`What kind of exercise do lazy people do?
+`What kind of exercise do lazy people do?<br>
 Diddly-squats.`,
 
-`Why don’t Calculus majors throw house parties?
+`Why don’t Calculus majors throw house parties?<br>
 Because you should never drink and derive.`,
 
-`What do you call a parade of rabbits hopping backwards?
+`What do you call a parade of rabbits hopping backwards?<br>
 A receding hare-line.`,
 
-`What does Charles Dickens keep in his spice rack?
+`What does Charles Dickens keep in his spice rack?<br>
 The best of thymes, the worst of thymes.`,
 
-`What’s the different between a cat and a comma?
+`What’s the different between a cat and a comma?<br>
 A cat has claws at the end of paws; A comma is a pause at the end of a clause. Don’t forget to bookmark these other “what’s the difference between” jokes that will crack you up.`,
 
-`Why should the number 288 never be mentioned?
+`Why should the number 288 never be mentioned?<br>
 It’s two gross.`,
 
-`What did the Tin Man say when he got run over by a steamroller?
+`What did the Tin Man say when he got run over by a steamroller?<br>
 “Curses! Foil again!”`,
 
-`What did the bald man exclaim when he received a comb for a present?
+`What did the bald man exclaim when he received a comb for a present?<br>
 Thanks— I’ll never part with it!`,
 
-`What did the Buddhist say to the hot dog vendor?
+`What did the Buddhist say to the hot dog vendor?<br>
 Make me one with everything.`,
 
-`What did the left eye say to the right eye?
+`What did the left eye say to the right eye?<br>
 Between you and me, something smells.`,
 
-`What do you call a fake noodle?
+`What do you call a fake noodle?<br>
 An impasta.`,
 
-`How do you make a tissue dance?
+`How do you make a tissue dance?<br>
 Put a little boogie in it.`,
 
-`What did the 0 say to the 8?
+`What did the 0 say to the 8?<br>
 Nice belt!`,
 
-`What do you call a pony with a cough?
+`What do you call a pony with a cough?<br>
 A little horse.`,
 
-`What did one hat say to the other?
+`What did one hat say to the other?<br>
 You wait here. I’ll go on a head. If you loved this, you’ll get a kick out of these dog puns.`,
 
-`What do you call a magic dog?
+`What do you call a magic dog?<br>
 A labracadabrador.`,
 
-`What did the shark say when he ate the clownfish?
+`What did the shark say when he ate the clownfish?<br>
 This tastes a little funny.`,
 
-`What’s orange and sounds like a carrot?
+`What’s orange and sounds like a carrot?<br>
 A parrot.`,
 
-`Why can’t you hear a pterodactyl go to the bathroom?
+`Why can’t you hear a pterodactyl go to the bathroom?<br>
 Because the “P” is silent.`,
 
-`What do you call a woman with one leg?
+`What do you call a woman with one leg?<br>
 Eileen.`,
 
-`What did the pirate say when he turned 80?
+`What did the pirate say when he turned 80?<br>
 Aye matey.`,
 
-`Why did the frog take the bus to work today?
+`Why did the frog take the bus to work today?<br>
 His car got toad away.`,
 
-`What did the buffalo say when his son left for college?
+`What did the buffalo say when his son left for college?<br>
 Bison.`,
 
-`What is an astronaut’s favorite part on a computer?
+`What is an astronaut’s favorite part on a computer?<br>
 The space bar.`,
 
-`Why did the yogurt go to the art exhibition?
+`Why did the yogurt go to the art exhibition?<br>
 Because it was cultured.`,
 
-`What do you call an apology written in dots and dashes?
+`What do you call an apology written in dots and dashes?<br>
 Re-Morse code.`,
 
-`Why did the hipster burn his mouth?
+`Why did the hipster burn his mouth?<br>
 He drank the coffee before it was cool.`,
 
-`Once my dog ate all the Scrabble tiles.
+`Once my dog ate all the Scrabble tiles.<br>
 He kept leaving little messages around the house.`,
 
-`I told my wife she was drawing her eyebrows too high.
+`I told my wife she was drawing her eyebrows too high.<br>
 She looked at me surprised.`,
 
-`Did you hear about the two people who stole a calendar?
+`Did you hear about the two people who stole a calendar?<br>
 They each got six months.`,
 
-`What’s Forest Gump’s password?
+`What’s Forest Gump’s password?<br>
 1Forest1.`,
 
-`How do poets say hello?
+`How do poets say hello?<br>
 Hey, haven’t we metaphor? If you’re a word nerd, here are 20 grammar jokes that are hilarious.`,
 
-`Where does Batman go to the bathroom?
+`Where does Batman go to the bathroom?<br>
 The batroom.`,
 
-`Why did the Oreo go to the dentist?
+`Why did the Oreo go to the dentist?<br>
 Because he lost his filling.`,
 
-`What do you get from a pampered cow?
+`What do you get from a pampered cow?<br>
 Spoiled milk.`,
 
-`Why is it annoying to eat next to basketball players?
+`Why is it annoying to eat next to basketball players?<br>
 They dribble all the time.`,
 
-`What breed of dog can jump higher than buildings?
+`What breed of dog can jump higher than buildings?<br>
 Any dog, because buildings can’t jump.`,
 
-`How many times can you subtract 10 from 100?
+`How many times can you subtract 10 from 100?<br>
 Once. The next time you would be subtracting 10 from 90.`,
 
-`Why did the M&M go to school?
+`Why did the M&M go to school?<br>
 It wanted to be a Smartie.`,
 
-`Why do bees have sticky hair?
+`Why do bees have sticky hair?<br>
 Because they use honeycombs.`,
 
-`How does a rabbi make his coffee?
+`How does a rabbi make his coffee?<br>
 Hebrews it.`,
 
-`I got my daughter a fridge for her birthday.
+`I got my daughter a fridge for her birthday.<br>
 I can’t wait to see her face light up when she opens it.`,
 
-`I poured root beer in a square glass.
+`I poured root beer in a square glass.<br>
 Now I just have beer.`,
 
-`Why aren’t koalas actual bears?
+`Why aren’t koalas actual bears?<br>
 They don’t meet the koalafications.`,
 
-`Rest in peace to boiling water.
+`Rest in peace to boiling water.<br>
 You will be mist.`,
 
-`What do you call a rooster staring at a pile of lettuce?
+`What do you call a rooster staring at a pile of lettuce?<br>
 A chicken sees a salad.`,
 
-`Why did the nurse need a red pen at work?
+`Why did the nurse need a red pen at work?<br>
 In case she needed to draw blood.`,
 
-`How do you throw a space party?
+`How do you throw a space party?<br>
 You planet.`,
 
-`The numbers 19 and 20 got into a fight.
+`The numbers 19 and 20 got into a fight.<br>
 21.`,
 
-`Why did it get so hot in the baseball stadium after the game?
+`Why did it get so hot in the baseball stadium after the game?<br>
 All of the fans left.`,
 
-`What do you call a train carrying bubblegum?
+`What do you call a train carrying bubblegum?<br>
 A chew-chew train.`,
 
-`Why did the math textbook visit the guidance counselor?
+`Why did the math textbook visit the guidance counselor?<br>
 It needed help figuring out its problems.`,
 
-`Why can’t male ants sink?
+`Why can’t male ants sink?<br>
 They’re buoy-ant.`,
 
-`Want to hear a construction joke?
+`Want to hear a construction joke?<br>
 Oh never mind, I’m still working on that one.`,
 
-`Talk is cheap?
+`Talk is cheap?<br>
 Have you ever talked to a lawyer?`,
 
-`Why did the gym close down?
+`Why did the gym close down?<br>
 It just didn’t work out!`,
 
-`Two artists had an art contest.
+`Two artists had an art contest.<br>
 It ended in a draw!`,
 
-`I tried to sue the airport for misplacing my luggage.
+`I tried to sue the airport for misplacing my luggage.<br>
 I lost my case.`,
 
-`I have a fear of speed bumps.
+`I have a fear of speed bumps.<br>
 But I am slowly getting over it.`,
 
-`Where do you find a cow with no legs?
+`Where do you find a cow with no legs?<br>
 Right where you left it.`,
 
-`What did one traffic light say to the other?
+`What did one traffic light say to the other?<br>
 Stop looking! I’m changing!`,
 
-`What type of sandals do frogs wear?
+`What type of sandals do frogs wear?<br>
 Open-toad!`,
 
-`Why was six afraid of seven?
+`Why was six afraid of seven?<br>
 Because seven ate nine.`,
 
-`What do you call a boomerang that doesn’t come back?
+`What do you call a boomerang that doesn’t come back?<br>
 A stick!`,
 
-`What starts with E, ends with E, and has only 1 letter in it?
+`What starts with E, ends with E, and has only 1 letter in it?<br>
 Envelope.`,
 
-`Why doesn’t the sun go to college?
+`Why doesn’t the sun go to college?<br>
 Because it has a million degrees!`,
 
-`How do you count cows?
+`How do you count cows?<br>
 With a cowculator.`,
 
-`Why are skeletons so calm?
+`Why are skeletons so calm?<br>
 Because nothing gets under their skin.`,
 
-`Why is England the wettest country?
+`Why is England the wettest country?<br>
 Because so many kings and queens have been reigning there.`,
 
-`Did you hear about the kidnapping at school?
+`Did you hear about the kidnapping at school?<br>
 It’s okay. He woke up.`,
 
-`What are shark’s two most favorite words?
+`What are shark’s two most favorite words?<br>
 Man overboard!`,
 
-`Can February march?
+`Can February march?<br>
 No, but April may.`,
 
-`Where does the sheep get his hair cut?
+`Where does the sheep get his hair cut?<br>
 The baa baa shop!`,
 
-`Why are ghosts such bad liars?
+`Why are ghosts such bad liars?<br>
 Because they are easy to see through.`,
 
-`Why does Humpty Dumpty love autumn?
+`Why does Humpty Dumpty love autumn?<br>
 Because Humpty Dumpty had a great fall.`,
 
-`Where do fish sleep?
+`Where do fish sleep?<br>
 In the riverbed.`,
 
-`How do trees get online?
+`How do trees get online?<br>
 They just log on!`,
 
-`What do you call a bear with no teeth?
+`What do you call a bear with no teeth?<br>
 A gummy bear.`,
 
-`Why couldn’t the leopard play hide and seek?
+`Why couldn’t the leopard play hide and seek?<br>
 Because he was always spotted.`];
-let any=Math.floor(Math.random()*101);
-console.log(jokes[any])
-// document.write(jokes[any])
+const find_new=()=>{
+    return Math.floor(Math.random()*101);
+}
+document.getElementById("joke-btn").onclick=find_new;
+let any=find_new();
+console.log(jokes[any]);
+document.write(jokes[any]);
