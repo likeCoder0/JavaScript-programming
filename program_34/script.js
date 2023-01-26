536fa885ec4f8a1f3d5b1cac85d105badb975c08
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-26 11:05:14 +0530

    let do it

--- program_34/script.js
+++ program_34/script.js
@@ -1,11 +1,1 @@
-console.log(console);
-console.error("error");
-console.info("info");
-console.warn("warn");
-console.assert("err"!=false);
-console.assert("err"==false);
-console.time("for_loop");
-for(let i=0;i<10;i++){
-    console.log(i);
-}
-console.timeEnd("for_loop");
+console.log(document.body.firstElementChild)
