d905af442688000f9eef319bb87e84c880da9679
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-27 13:22:42 +0530

    innerHTML AND outerHTML

---
+++ program_41/script.js
@@ -0,0 +1,15 @@
+// let x=document.getElementsByTagName("span")[0];
+// console.log(x);
+// let y=document.getElementsByTagName("span")[0];
+// console.dir(y);
+// console.log(document.body.firstChild.nodeName);
+// console.log(document.body.firstElementChild.nodeName);
+
+// first.innerHTML;
+// first.innerHTML="<i>hey , i am a italic";
+// first.outerHTML;
+// first.outerHTML='<span id="first"><i>hey , i am a span</i></span>';
+
+// document.body.firstChild.data
+// $0.hidden=false
+console.log(document.body.textContent);
