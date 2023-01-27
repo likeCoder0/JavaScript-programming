77ae49d3c4900f270659a5e8346ca6c6fac2046c
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-27 13:47:55 +0530

    Attribute method

---
+++ program_42/script.js
@@ -0,0 +1,11 @@
+let x=first.getAttribute("class");
+// console.log(x);
+// console.log(first.hasAttribute("class"));
+// console.log(first.hasAttribute("style"));
+// first.setAttribute("hidden","true");
+// first.setAttribute("class","true anchal");
+// first.removeAttribute("class");
+// console.log(first.attributes);
+console.log(first.dataset)
+console.log(first.dataset.game)
+console.log(first.dataset.player)
