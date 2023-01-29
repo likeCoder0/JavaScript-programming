0bc55b8ef2792929a95be9cad44b8f017d0f92b7
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-29 14:19:00 +0530

    try and catch

---
+++ program_60/script.js
@@ -0,0 +1,15 @@
+setTimeout(() => {
+    console.log("Hacking wifi... Please wait...")
+}, 1000);
+try{
+    console.log(jay)
+}
+catch(error){
+    console.log("Balle balle")
+}
+setTimeout(() => {
+    console.log("fetching username and password... Please wait...")
+}, 2000);
+setTimeout(() => {
+    console.log("Hacking jay facebook id... Please wait...")
+}, 3000);
