e2971f6d8a39900ff791bf45aabe992347aeb921
likeCoder0 <2021.anchal.sharma@ves.ac.in>
2023-01-29 17:42:15 +0530

    finally

---
+++ program_62/script.js
@@ -0,0 +1,18 @@
+const f=()=>{
+try {
+    let a=0;    
+    console.log(program);
+    console.log("Program run successfully.");
+    return;    
+} catch (error) {
+    console.log("This is an error");
+    console.log(p)
+}
+finally{
+    console.log("I am a good boy.");
+    //closed the file
+    //exit the loop
+    //write the log file
+}
+}
+f()
